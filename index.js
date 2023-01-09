ef22f5f4ac5f588f36d62b5f30aafd173c066f37
Erik Tonnesen <estonnesen@gmail.com>
2023-01-09 12:52:19 -0500

    began adding functions within each class

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ listingsDiv.classList.add('listings')
 
 class Listings {
     constructor(){
-        this.listing = []
+        this.listing = [];
     }
 
     render() {
+        // this is simply rendering a div that will house each entry object
+        const listingsRender = document.createElement('div')
+        listingsRender.classList.add('listings_render')
+        listingsDiv.append(listingsRender)
 
+        return listingsRender
     }
 
     addListing() {
+        // going to push new entries into this.listing array
+        // addListing should add new instances of UserEntry
 
     }
 
@@ -19,6 +26,8 @@ class Listings {
 
     }
 }
+
+
 
 class UserEntry {
     constructor(address, propType, city, state) {
@@ -29,6 +38,27 @@ class UserEntry {
     }
 
     render() {
+        // this is rendering a ul that is housing each property of the user entry
+        const userWrapper = document.createElement('ul')
+        // listingsDiv.append(userWrapper)
 
+        const addressLi = document.createElement('li')
+        userWrapper.appendChild(addressLi)
+        const propTypeLi = document.createElement('li')
+        userWrapper.appendChild(propTypeLi)
+        const cityLi = document.createElement('li')
+        userWrapper.appendChild(cityLi)
+        const stateLi = document.createElement('li')
+        userWrapper.appendChild(stateLi)
+
+        return userWrapper
     }
 }
+
+const testUser = new UserEntry("123 Street Lane", "Apt", "Brooklyn", "New York");
+
+testUser.render()
+console.log(testUser)
+
+const masterListing = new Listings
+document.body.append(masterListing.render())
