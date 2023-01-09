121eec3628534123bc425cdbe16158f5d294ce89
Erik Tonnesen <estonnesen@gmail.com>
2023-01-09 12:13:12 -0500

    created classes and began DOM implimentation

---
+++ index.js
@@ -0,0 +1,34 @@
+const listingsDiv = document.createElement('div')
+listingsDiv.classList.add('listings')
+
+
+class Listings {
+    constructor(){
+        this.listing = []
+    }
+
+    render() {
+
+    }
+
+    addListing() {
+
+    }
+
+    removeListing() {
+
+    }
+}
+
+class UserEntry {
+    constructor(address, propType, city, state) {
+        this.address = address, 
+        this.propType = propType, 
+        this.city = city, 
+        this.state = state
+    }
+
+    render() {
+
+    }
+}
